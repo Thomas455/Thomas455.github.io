7ce2405a59c6dbe613291c88d9d7f0b089166c5c
逗比Thomas <gzdaikin@iCloud.com>
2026-08-29 12:02:43 +0800

    升级统计功能

---
+++ assets/js/baidu-analytics.js
@@ -0,0 +1,8 @@
+/* 百度统计（tongji.baidu.com）· Thomas455 全站流量统计 */
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b27042a7b1bc987eb347a45797bd2c1f";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
